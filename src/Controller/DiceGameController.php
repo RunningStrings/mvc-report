fe4a4c8691c3dbb8604aa4b81dc6f47e2053675d
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-04-15 21:05:51 +0200

    Kmom02 exercises done

--- src/Controller/DiceGameController.php
+++ src/Controller/DiceGameController.php
@@ -7,7 +7,9 @@ use App\Dice\DiceGraphic;
 use App\Dice\DiceHand;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Yaml\Yaml;
 
@@ -35,11 +37,10 @@ class DiceGameController extends AbstractController
         $data = [
             "dice" => $die->roll(),
             "diceString" => $die->getAsString(),
+            "metadata" => $this->loadMetaData(),
         ];
 
-        $metadata = $this->loadMetaData();
-
-        return $this->render('pig/test/roll.html.twig', $data, $metadata);
+        return $this->render('pig/test/roll.html.twig', $data);
     }
 
     #[Route("/game/pig/test/roll/{num<\d+>}", name: "test_roll_num_dices")]
@@ -60,10 +61,8 @@ class DiceGameController extends AbstractController
         $data = [
             "num_dices" => count($diceRoll),
             "diceRoll" => $diceRoll,
+            "metadata" => $this->loadMetaData(),
         ];
-
-        $metadata = $this->loadMetaData();
-        $data = array_merge($data, ['metadata' => $metadata]);
 
         return $this->render('pig/test/roll_many.html.twig', $data);
     }
@@ -89,10 +88,116 @@ class DiceGameController extends AbstractController
         $data = [
             "num_dices" => $hand->getNumberDices(),
             "diceRoll" => $hand->getString(),
+            "metadata" => $this->loadMetaData(),
         ];
 
-        $metadata = $this->loadMetaData();
+        return $this->render('pig/test/dicehand.html.twig', $data);
+    }
 
-        return $this->render('pig/test/dicehand.html.twig', $data, $metadata);
+    #[Route("/game/pig/init", name: "pig_init_get", methods: ['GET'])]
+    public function init(): Response
+    {
+        $data = [
+            "metadata" => $this->loadMetaData(),
+        ];
+        return $this->render('pig/init.html.twig', $data);
+    }
+
+    #[Route("/game/pig/init", name: "pig_init_post", methods: ['POST'])]
+    public function initCallback(
+        Request $request,
+        SessionInterface $session
+    ): Response
+    {
+        $numDice = $request->request->get('num_dices');
+
+        $hand = new DiceHand();
+        for ($i = 1; $i <= $numDice; $i++) {
+            $hand->add(new DiceGraphic());
+        }
+        $hand->roll();
+
+        $session->set("pig_dicehand", $hand);
+        $session->set("pig_dices", $numDice);
+        $session->set("pig_round", 0);
+        $session->set("pig_total", 0);
+
+        $data = [
+            "metadata" => $this->loadMetaData(),
+        ];
+
+        return $this->redirectToRoute('pig_play', $data);
+    }
+
+    #[Route("/game/pig/play", name: "pig_play", methods: ['GET'])]
+    public function play(
+        SessionInterface $session
+    ): Response
+    {
+        $dicehand = $session->get("pig_dicehand");
+
+        $data = [
+            "pigDices" => $session->get("pig_dices"),
+            "pigRound" => $session->get("pig_round"),
+            "pigTotal" => $session->get("pig_total"),
+            "diceValues" => $dicehand->getString(),
+            "metadata" => $this->loadMetaData(),
+        ];
+
+        return $this->render('pig/play.html.twig', $data);
+    }
+
+    #[Route("/game/pig/roll", name: "pig_roll", methods: ['POST'])]
+    public function roll(
+        SessionInterface $session
+    ): Response
+    {
+        $hand = $session->get("pig_dicehand");
+        $hand->roll();
+
+        $roundTotal = $session->get("pig_round");
+        $round = 0;
+        $values = $hand->getValues();
+        foreach ($values as $value) {
+            if ($value === 1) {
+                $round = 0;
+                $roundTotal = 0;
+                $this->addFlash(
+                    'warning',
+                    'You got a 1 and you lost the round points!'
+                );
+                break;
+            }
+            $round += $value;
+        }
+
+        $session->set("pig_round", $roundTotal + $round);
+
+        // $data = [
+        //     "metadata" => $this->loadMetaData(),
+        // ];
+
+        return $this->redirectToRoute('pig_play');
+    }
+
+    #[Route("/game/pig/save", name: "pig_save", methods: ['POST'])]
+    public function save(
+        SessionInterface $session
+    ): Response
+    {
+        $roundTotal = $session->get("pig_round");
+        $gameTotal = $session->get("pig_total");
+        $session->set("pig_round", 0);
+        $session->set("pig_total", $roundTotal + $gameTotal);
+        $this->addFlash(
+            'notice',
+            'Your round was saved to the total!'
+        );
+
+        // $data = [
+        //     "metadata" => $this->loadMetaData(),
+        // ];
+
+        return $this->redirectToRoute('pig_play');
     }
 }
