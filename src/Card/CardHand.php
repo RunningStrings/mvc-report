cf0d03648c8e278dd88b437bb09331f93693151d
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-04-19 01:33:58 +0200

    Added class CardHand

--- src/Card/CardHand.php
+++ src/Card/CardHand.php
@@ -4,5 +4,26 @@ namespace App\Card;
 
 class CardHand
 {
-    // CardHand functions
+    protected $cards = [];
+    protected $deck;
+
+    public function __construct(DeckOfCards $deck)
+    {
+        $this->deck = $deck;
+    }
+
+    public function addCard(Card $card)
+    {
+        $this->cards[] = $card;
+    }
+
+    public function getHand()
+    {
+        return $this->cards;
+    }
+
+    public function clearHand()
+    {
+        $this->cards = [];
+    }
 }
