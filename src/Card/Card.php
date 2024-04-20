cbbb3c84eff7483bb3decf5c734e29db3f0f1f51
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-04-20 20:24:21 +0200

    Added return type declarations

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -7,23 +7,23 @@ class Card
     protected $suit;
     protected $value;
 
-    public function __construct($suit, $value)
+    public function __construct(string $suit, string $value)
     {
         $this->suit = $suit;
         $this->value = $value;
     }
 
-    public function getSuit()
+    public function getSuit(): string
     {
         return $this->suit;
     }
 
-    public function getValue()
+    public function getValue(): string
     {
         return $this->value;
     }
 
-    public function __toString()
+    public function __toString(): string
     {
         return $this->value . ' of ' . $this->suit;
     }
