b896223ec8d049216611f78c9f0deeb0ff0b6d75
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-04-20 20:33:20 +0200

    Add functionality for dealing cards
    Added route for dealing cards to players:
    - /api/deck/deal/{players<\d+>}/{cards<\d+>}
    
    In API template, for routes with "player" in path, added creation of
    form with player input, card input and button.

--- src/Controller/CardGameApiController.php
+++ src/Controller/CardGameApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Card\CardHand;
 use App\Card\DeckOfCards;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,12 +14,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CardGameApiController extends AbstractController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     #[Route(
         "/api/deck",
         name: "api_deck",
         methods: ['GET'],
-        options: ['description' => 'Get a sorted deck of cards.']
+        options: ['description' => 'Visar en komplett, sorterad kortlek och lagrar den i sessionen.']
         )]
     public function jsonDeck(
         SessionInterface $session
@@ -43,7 +50,7 @@ class CardGameApiController extends AbstractController
         "/api/deck/shuffle",
         name: "api_shuffle",
         methods: ['POST'],
-        options: ['description' => 'Shuffles and returns a deck of cards and saves it to the session.']
+        options: ['description' => 'Blandar och visar en kortlek, och lagrar den i sessionen.']
     )]
     public function jsonShuffle(
         SessionInterface $session
@@ -75,7 +82,7 @@ class CardGameApiController extends AbstractController
         "/api/deck/draw",
         name: "api_draw",
         methods: ['POST'],
-        options: ['description' => 'Draws 1 card from the deck of cards in the session.']
+        options: ['description' => 'Drar 1 kort från kortleken i sessionen.']
     )]
     public function jsonDraw(
         SessionInterface $session,
@@ -91,7 +98,7 @@ class CardGameApiController extends AbstractController
 
         $card = $deck->draw();
         if (!$card) {
-            $response = new JsonResponse('No cards left in deck.');
+            $response = new JsonResponse('Inga kort kvar i leken.');
                 $response->setEncodingOptions(
                     $response->getEncodingOptions() | JSON_PRETTY_PRINT
                 );
@@ -118,7 +125,8 @@ class CardGameApiController extends AbstractController
         "/api/deck/draw/{number<\d+>}",
         name:"api_draw_number",
         methods: ['POST'],
-        options: ['description' => 'Draws the specified number of cards from the deck of cards in the session.'])]
+        options: ['description' => 'Drar det valda antalet kort från kortleken i sessionen.']
+    )]
     public function jsonDrawNum(
         Request $request,
         SessionInterface $session
@@ -139,11 +147,11 @@ class CardGameApiController extends AbstractController
             if ($drawnCard) {
                 $drawnCards[] = $drawnCard->getValue() . ' of ' . $drawnCard->getSuit();
             } else {
-                $response = new JsonResponse('No cards left in deck.');
+                $response = new JsonResponse('Inga kort kvar i leken.');
                 $response->setEncodingOptions(
                     $response->getEncodingOptions() | JSON_PRETTY_PRINT
                 );
-        
+
                 return $response;
             }
         }
@@ -160,8 +168,71 @@ class CardGameApiController extends AbstractController
             $response->getEncodingOptions() | JSON_PRETTY_PRINT
         );
 
-            return $response;
+        return $response;
+    }
+
+    #[Route(
+        "/api/deck/deal/{players<\d+>}/{cards<\d+>}",
+        name: "api_deal_cards",
+        methods: ['POST'],
+        options: ['description' => 'Delar ut det valda antalet kort till det valda antalet spelare från kortleken i sessionen.']
+    )]
+    public function jsonDealCards(
+        Request $request,
+        SessionInterface $session
+    ): Response 
+    {
+        $cards = $request->request->get('cards');
+        $players = $request->request->get('players');
+        $deck = $session->get("deck");
+
+        $this->logger->info('Value of $cards: ' . $cards);
+        $this->logger->info('Value of $players: ' . $players);
+
+        if (!$deck) {
+            $deck = new DeckOfCards();
+            $deck->shuffleDeck();
+            $session->set("deck", $deck);
         }
 
+        $playerHands = [];
+        for ($i = 0; $i < $players; $i++) {
+            $playerHands["Spelare " . ($i + 1)] = new CardHand($deck);
+        }
 
+        for ($i = 0; $i < $cards; $i++) {
+            foreach ($playerHands as $player => $hand) {
+                $card = $deck->draw();
+                if ($card) {
+                    $hand->addCard($card);
+                } else {
+                    $response = new JsonResponse('Inga kort kvar i leken.');
+                    $response->setEncodingOptions(
+                        $response->getEncodingOptions() | JSON_PRETTY_PRINT
+                    );
+
+                    return $response;
+                }
+            }
+        }
+
+        $remainingCards = count($deck->getDeck());
+
+        foreach ($playerHands as $player => $hand) {
+            $playerHands[$player] = $hand->toHandArray();
+        }
+
+        $data = [
+            "playerHands" => $playerHands,
+            "remainingCards" => $remainingCards,
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
     }
+
+}
