f8aa8bd9520e82a373b0418c6b0260bd7ade17b3
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-05-11 01:58:36 +0200

    Add routes for the card game
    Added route for initiating the game, route for the playing field,
    route for the user to draw a card (hit), and route for the user to
    stand and trigger the bank's turn (stand).

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Card\CardHand;
+use App\Card\DeckOfCards;
 use App\Game\Game;
+use App\Game\Player;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -29,7 +33,7 @@ class CardGameController extends AbstractController
         return $this->render('game/home.html.twig', $data);
     }
 
-    #[Route("/game/doc", name: "doc")]
+    #[Route("/game/doc", name: "game_doc")]
     public function doc(): Response
     {
         $data = [
@@ -38,6 +42,71 @@ class CardGameController extends AbstractController
         ];
 
         return $this->render('game/doc.html.twig', $data);
+    }
+
+    #[Route("/game/init", name: "game_init")]
+    public function init(SessionInterface $session): Response
+    {
+        $deck = new DeckOfCards();
+        $deck->shuffleDeck();
+        $session->set("deck", $deck);
+
+        $player = new Player('Player');
+        $playerHand = new CardHand($deck);
+        $player->addHand($playerHand);
+        $session->set("player", $player);
+
+        $bank = new Player('Bank');
+        $bankHand = new CardHand($deck);
+        $bank->addHand($bankHand);
+        $session->set("bank", $bank);
+
+        $game = new Game($deck, $player, $bank);
+        $session->set("game", $game);
+
+        return $this->redirectToRoute('game_play');
+    }
+
+    #[Route("/game/play", name: "game_play", methods: ['GET'])]
+    public function play(SessionInterface $session): Response
+    {
+        $game = $session->get('game');
+
+        $player = $game->getPlayer();
+
+        $data = [
+            "title" => "Tjugoett",
+            "player" => $player,
+            "metadata" => $this->loadMetaData()
+        ];
+
+        return $this->render('game/play.html.twig', $data);
+    }
+
+    #[Route("/game/hit", name:"game_hit", methods: ['GET'])]
+    public function hit(
+        SessionInterface $session
+    ): Response {
+        $game = $session->get("game");
+
+        $game->playerTurn();
+
+        $session->set("game", $game);
+
+        return $this->redirectToRoute('game_play');
+    }
+
+    #[Route("/game/stand", name:"game_stand", methods: ['GET'])]
+    public function stand(
+        SessionInterface $session
+    ): Response {
+        $game = $session->get("game");
+
+        $game->bankTurn();
+
+        $session->set("game", $game);
+
+        return $this->redirectToRoute('game_play');
     }
 
         /**
